1ac01f54ace65ce1ae3b4fb6f260707d91f56f09
Daniela Monterrubio <194921@iberopuebla.mx>
2023-10-22 15:46:24 -0600

    Practica5

--- 5practica/files/Codigo/Codigo.ino
+++ 5practica/files/Codigo/Codigo.ino
@@ -1,12 +1,3 @@
-
-/*      ATTINY85                            Out/10/2022          Esdras Silva
-    Programa Display de Temperatura NTC100K a 25C 
-     oled.setFont(FONT6X8);
-     oled.setFontX2(FONT6X8);
-     oled.setFont(FONT8X16);
-     oled.setFont(FONT8X16);
- */
-
 #include <TinyWireM.h>
 #include <Tiny4kOLED.h>
 
@@ -20,7 +11,7 @@ void prepareDisplay() {
 }
 
 void setup() {
-  oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);//oled.setRotation(0);
+  oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
  // Two fonts are supplied with this library, FONT8X16 and FONT6X8
  // To clear all the memory
     oled.clear();
@@ -32,15 +23,13 @@ void setup() {
 void loop() {
   float rpm=1234.7;
   // Checks 1 second passed
-  if (digitalRead(3)==HIGH) {
-      // Print to display
-      oled.setCursor(48, 0);
-      oled.print("ON ");
-    }
-    else{
-      oled.setCursor(48, 0);
-      oled.print("OFF");
-    }
+  if (digitalRead(3) == HIGH){
+    // Print to display
+    oled.setCursor(48, 0);
+    oled.print("ON ");
   }
-
-  
+  else{
+    oled.setCursor(48, 2);
+    oled.print("OFF");
+  }
+}
